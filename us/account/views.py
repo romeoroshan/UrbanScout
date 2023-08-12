4b1ecf01cb230f29d550c3416f67190e68c55c2c
Romeo <romeoroshan2024@mca.ajce.in>
2023-08-12 12:42:18 +0530

    admin page

--- us/account/views.py
+++ us/account/views.py
@@ -7,6 +7,7 @@ from .models import Pos_Choice, District_Choice
 from datetime import date
 # Create your views here.
 def index(request):
+    
     return render(request,'index.html')
 def login(request):
     return render(request,'login.html')
